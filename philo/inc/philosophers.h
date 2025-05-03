be80f32131efae75199159eb3d8819993d3d979f
ozamora- <ozamora-@student.42madrid.com>
2025-05-03 23:34:58 +0200

    refactor: Update initialization functions and structure for philosopher simulation

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,28 +6,29 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 23:46:11 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/05/03 22:49:12 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/05/03 23:22:30 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-# include <string.h>	// To use memset
-# include <stdio.h>		// To use printf
-# include <stdlib.h>	// To use malloc, free
-# include <unistd.h>	// To use write, usleep
-# include <sys/time.h>	// To use gettimeofday
-# include <pthread.h>	// To use pthread_create, pthread_detach, pthread_join
+# include <pthread.h>  // To use pthread_create, pthread_detach, pthread_join
+# include <stdio.h>    // To use printf
+# include <stdlib.h>   // To use malloc, free
+# include <string.h>   // To use memset
+# include <sys/time.h> // To use gettimeofday
+# include <unistd.h>   // To use write, usleep
 						// pthread_mutex_init, pthread_mutex_destroy
 						// pthread_mutex_lock, pthread_mutex_unlock
 # include <limits.h>
 
-#ifndef MAX_PHILO
-# define MAX_PHILO 200
-#endif
+# ifndef MAX_PHILO
+#  define MAX_PHILO 200
+# endif
 
-# define USAGE "Usage:\t ./philo \
+# define USAGE \
+	"Usage:\t ./philo \
 	<number_of_philos> <time_to_die> <time_to_eat> <time_to_sleep> \
 	[<number_of_times_each_philosopher_must_eat>]"
 # define EXAMPLE "Example: ./philo 3 400 200 200"
@@ -43,30 +44,38 @@
 typedef struct s_philo
 {
 	int				id;
-	unsigned int	num_eat;
-	pthread_t		thread_id;
+	pthread_t		philo_thread;
 	pthread_mutex_t	*l_fork;
 	pthread_mutex_t	*r_fork;
-	unsigned long	t_last_meal;
+	unsigned long	lastmeal_time;
+	pthread_mutex_t	meal_mutex;
+	int				meals_eaten;
 }					t_philo;
 
 typedef struct s_admin
 {
-	struct timeval	t_start;
-	struct timeval	t_end;
-	unsigned long	t_elapsed;
 	unsigned int	num_philo;
 	unsigned int	time_to_die;
 	unsigned int	time_to_eat;
 	unsigned int	time_to_sleep;
 	unsigned int	num_eat;
-	pthread_t		*admin_thread;
-	struct t_philo	*philo_thread;
+	pthread_t		admin_thread;
+	t_philo			*philos;
 	pthread_mutex_t	*forks;
+	pthread_mutex_t	sim_mutex;
+	pthread_mutex_t	print_mutex;
 	int				sim_active;
+	unsigned long	start_time;
 }					t_admin;
 
-long	ft_atol(const char *nptr);
-int		check_args_int(char **args);
+int					init_admin(int argc, char **argv, t_admin *data);
+int					init_philos(t_admin *data);
+unsigned long		get_current_time(t_admin *data);
+void				*admin_routine(void *arg);
+void				*philo_routine(void *arg);
+void				clean_and_destroy(t_admin *data);
+
+long				ft_atol(const char *nptr);
+int					check_args_int(char **args);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 23:47:31 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/05/03 22:48:50 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/05/03 23:34:43 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	init_admin(int argc, char **argv, t_admin *data)
 {
-	gettimeofday(&data->t_start, NULL);
+	struct timeval	start_time;
+	unsigned int	i;
+
 	if (!my_is_unsigned_nbr(argv[1]) || !my_is_unsigned_nbr(argv[2])
 		|| !my_is_unsigned_nbr(argv[3]) || !my_is_unsigned_nbr(argv[4])
 		|| (argc == 6 && !my_is_unsigned_nbr(argv[5])))
@@ -26,19 +28,50 @@ int	init_admin(int argc, char **argv, t_admin *data)
 	data->num_eat = UINT_MAX;
 	if (argc == 6)
 		data->num_eat = my_atoui(argv[5]);
-	data->philo_thread = malloc(data->num_philo * sizeof(pthread_t *));
-	data->forks = malloc(data->num_philo * sizeof(pthread_mutex_t *));
 	data->sim_active = TRUE;
-	pthread_create(data->admin_thread, NULL, admin_routine, data);
+	if (gettimeofday(&start_time, NULL) != 0)
+		return (FALSE);
+	data->start_time = start_time.tv_sec * SEC_TO_USEC + start_time.tv_usec;
+	data->forks = malloc(data->num_philo * sizeof(pthread_mutex_t));
+	if (!data->forks)
+		return (FALSE);
+	i = -1;
+	while (++i < data->num_philo)
+		pthread_mutex_init(&data->forks[i], NULL);
+	pthread_mutex_init(&data->sim_mutex, NULL);
+	pthread_mutex_init(&data->print_mutex, NULL);
+	if (!init_philos(data))
+		return (free(data->forks), (FALSE));
 	return (TRUE);
 }
 
-unsigned long	calc_elapsed_time(t_admin *data)
+int	init_philos(t_admin *data)
 {
-	gettimeofday(&data->t_end, NULL);
-	data->t_elapsed = (data->t_end.tv_sec - data->t_start.tv_sec) * SEC_TO_USEC
-		+ (data->t_end.tv_usec - data->t_start.tv_usec);
-	return (data->t_elapsed);
+	data->philos = malloc(data->num_philo * sizeof(t_philo));
+	if (!data->philos)
+		return (FALSE);
+	for (unsigned int i = 0; i < data->num_philo; i++)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].l_fork = &data->forks[i];
+		data->philos[i].r_fork = &data->forks[(i + 1) % data->num_philo];
+		data->philos[i].lastmeal_time = data->start_time;
+		data->philos[i].meals_eaten = 0;
+		pthread_mutex_init(&data->philos[i].meal_mutex, NULL);
+	}
+	return (TRUE);
+}
+
+unsigned long	get_current_time(t_admin *data)
+{
+	struct timeval	current_time;
+	unsigned long	elapsed_time;
+
+	if (gettimeofday(&current_time, NULL) != 0)
+		return (0);
+	elapsed_time = ((current_time.tv_sec * SEC_TO_USEC + current_time.tv_usec)
+			- data->start_time) / MSEC_TO_USEC;
+	return (elapsed_time);
 }
 
 void	*admin_routine(void *arg)
@@ -58,12 +91,22 @@ void	*philo_routine(void *arg)
 int	main(int argc, char **argv)
 {
 	t_admin	data;
+	int		i;
 
 	if (argc <= 5 || argc > 6)
 		printf("%s\n%s\n", USAGE, EXAMPLE);
 	if (init_admin(argc, argv, &data) == FALSE)
 		return (printf("%s\n", ERR_INPUT), EXIT_FAILURE);
+	i = 0;
+	while (i < data.num_philo)
+	{
+		pthread_create(&data.philos[i].philo_thread, NULL, philo_routine,
+			&data.philos[i]);
+		i++;
+	}
+	if (pthread_create(&data.admin_thread, NULL, admin_routine, &data) != 0)
+		return (free(data.philos), free(data.forks), FALSE);
 	pthread_join(data.admin_thread, NULL);
-	clean_and_destroy();
+	clean_and_destroy(&data);
 	return (0);
 }
